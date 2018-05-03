1ec31b0a8edc98e28cb59a5df97c161479ef2c47
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2018-05-03 14:09:29 -0300

    Controlando o fluxo do script

--- hello.go
+++ hello.go
@@ -1,19 +1,44 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
+}
+
+func exibeIntroducao() {
 	nome := "Rodolfo"
 	versao := 1.1
 	fmt.Println("Olá sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
+}
 
-	fmt.Println("1 - Iniciar monitoramento")
-	fmt.Println("2 - Exibir logs")
-	fmt.Println("0 - Sair do programa")
+func leComando() int {
+	var comandoLido int
+	fmt.Scanf("%d", &comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
 
-	var comando int
-	fmt.Println("O endereço da minha variável comando é:", &comando)
-	fmt.Scanf("%d", &comando)
-	fmt.Println("O comando escolhido foi", comando)
+	return comandoLido
 }
