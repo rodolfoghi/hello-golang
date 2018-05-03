57f6872bd64bc47861a008fe9f4ba4523e0e597d
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2018-05-03 16:34:47 -0300

    Fazendo requisições para a web

--- hello.go
+++ hello.go
@@ -2,23 +2,26 @@ package main
 
 import "fmt"
 import "os"
+import "net/http"
 
 func main() {
 	exibeIntroducao()
-	exibeMenu()
-	comando := leComando()
+	for {
+		exibeMenu()
+		comando := leComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo logs...")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			inicarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -41,4 +44,16 @@ func leComando() int {
 	fmt.Println("O comando escolhido foi", comandoLido)
 
 	return comandoLido
+}
+
+func inicarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://github.com"
+	res, _ := http.Get(site)
+
+	if res.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status code:", res.StatusCode)
+	}
 }
