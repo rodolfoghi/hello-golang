ea383c5dcc6433736596f80a69ad7e63c9ecc39b
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2018-05-04 14:18:13 -0300

    Lendo arquivos

--- hello.go
+++ hello.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -55,7 +58,7 @@ func leComando() int {
 
 func inicarMonitoramento() {
 	fmt.Println("Monitorando...")
-	sites := []string{"https://github.com", "https://minecraft.net/pt-br/", "https://code.visualstudio.com/", "https://www.mozilla.org/pt-BR/firefox/new/", "http://random-status-code.herokuapp.com/"}
+	sites := leSitesDoArquivo()
 
 	for i := 0; i < monitoramentos; i++ {
 		for _, site := range sites {
@@ -67,11 +70,43 @@ func inicarMonitoramento() {
 }
 
 func monitoraSite(site string) {
-	res, _ := http.Get(site)
+	res, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+		os.Exit(-1)
+	}
 
 	if res.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
 	} else {
 		fmt.Println("Site:", site, "está com problemas. Status code:", res.StatusCode)
 	}
+}
+
+func leSitesDoArquivo() []string {
+	var sites []string
+
+	arquivo, err := os.Open("sites.txt")
+	//arquivo, err := ioutil.ReadFile("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+		os.Exit(-1)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	arquivo.Close()
+	return sites
 }
